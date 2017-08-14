01f63e3d83bb541a18bb47e7c5d3575f961230ee
Frank Strieter <Strulle@users.noreply.github.com>
2017-08-14 11:49:47 +0200

    Create environment.ts

---
+++ app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    sendmailURL: 'url/to/mailservice',
+    articleDataURL: 'url/to/articledata',
+    articleImageURL: 'url/to/articleima',
+    receiver: 'receiver@example.com'
+}
